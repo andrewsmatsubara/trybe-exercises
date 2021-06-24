a37847dd122ccb0391a97256a2663ad0427beae9
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-06-24 19:17:09 -0300

    Exercicio 7

--- exercises/exercises/introdicao-a-javascript-e-logica-de-programacao_2/scripts-ex6.js
+++ exercises/exercises/introdicao-a-javascript-e-logica-de-programacao_2/scripts-ex6.js
@@ -1,5 +1,3 @@
-/*Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";*/
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let impar = 0;
 let numeroImpar = 0;
