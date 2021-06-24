7f1b4df206fdda841f04074fd93d6e669ae916aa
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-06-24 20:47:20 -0300

    Exercicio bonus 3

---
+++ exercises/exercises/introdicao-a-javascript-e-logica-de-programacao_2/scripts-bonus3.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let res = numbers[0];
+
+for(let index = 0; index <= numbers.length - 1; index += 1){
+        res = numbers[index] * numbers[index + 1];
+        console.log(res);
+    }
+
+    
