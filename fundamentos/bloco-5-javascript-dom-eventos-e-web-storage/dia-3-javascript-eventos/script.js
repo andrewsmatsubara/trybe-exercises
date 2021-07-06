56e3d197491632b574b8c4960b7d22d4fe5672d0
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-07-06 20:15:36 -0300

    Exercicio 1 realizado

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -12,4 +12,33 @@ function createDaysOfTheWeek() {
   };
   
   createDaysOfTheWeek();
-  
+
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  function createDaysOfTheMonth(){
+      let daysID = document.querySelector('#days');
+
+      for(let index = 0; index < dezDaysList.length; index += 1){
+          let day = dezDaysList[index];
+          let dayListItem = document.createElement('li');
+
+          if(day === 24 || day === 25 || day === 31){
+              dayListItem.className = 'day holiday';
+              dayListItem.innerHTML = day;
+              daysID.appendChild(dayListItem);
+          }else if(day === 4 || day === 11 || day === 18 || day === 25){
+              dayListItem.className = 'day friday';
+              dayListItem.innerHTML = day;
+              daysID.appendChild(dayListItem);
+          }else if(day === 25){
+              dayListItem.className = 'day holiday friday';
+              dayListItem.innerHTML = day;
+              daysID.appendChild(dayListItem);
+          }else{
+              dayListItem.className = 'day';
+              dayListItem.innerHTML = day;
+              daysID.appendChild(dayListItem);
+          }
+      }
+  }
+  createDaysOfTheMonth();
