c4ebf4fc06f37db05fea373e6aa4b9aa17801776
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-07-08 00:32:16 -0300

    Funçoes realizadas

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -47,11 +47,37 @@ function changeFontSize(){
 }
 
 function changeLineHeight(){
+    let heightBetween = document.querySelector('.btn-line-height');
+    let paragraph = document.getElementsByTagName('p');
+    let originalHeight = '20px';
+    let anotherHeight = '30px';
 
+    heightBetween.addEventListener('click', function(){
+        for(let index = 0; index < paragraph.length; index += 1){
+            if(paragraph[index].style.lineHeight === anotherHeight){
+                paragraph[index].style.lineHeight = originalHeight;
+            }else{
+                paragraph[index].style.lineHeight = anotherHeight;
+            }
+        }
+    });
 }
 
 function changeFontFamily(){
+    let fontStyle = document.querySelector('.btn-font-family');
+    let paragraph = document.getElementsByTagName('p');
+    let originalFamily = 'arial';
+    let anotherFamily = 'verdana';
 
+    fontStyle.addEventListener('click', function(){
+        for(let index = 0; index < paragraph.length; index += 1){
+            if(paragraph[index].style.fontFamily === anotherFamily){
+                paragraph[index].style.fontFamily = originalFamily;
+            }else{
+                paragraph[index].style.fontFamily = anotherFamily;
+            }
+        }
+    });
 }
 
 changeBackgroundColor();
