5d4c10aee20075ed2a4eeb7d68a98acebd192b81
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-07-14 22:38:27 -0300

    Exercicios 1 e 2 realizados

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,11 +1,19 @@
-let estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
+function criaOption() {
+	let estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
 
-let estado = document.querySelector('#estados');
+	let estado = document.querySelector('#estados');
 
-for (let index = 0; index < estados.length; index += 1) {
-	let option = document.createElement('option');
+	for (let index = 0; index < estados.length; index += 1) {
+		let option = document.createElement('option');
 
-	option.innerHTML = estados[index];
+		option.innerHTML = estados[index];
 
-	estado.appendChild(option);
+		estado.appendChild(option);
+	}
 }
+criaOption();
+
+function enviarDados(event){
+	event.preventDefault();	
+}
+enviarDados();
