e39ac655c91a5b7348cc63d032a8c678256985e1
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-07-15 19:12:46 -0300

    Exercicio 1 realizado

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,13 @@
+let estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
+
+let estado = document.querySelector('#estados');
+
+for (let index = 0; index < estados.length; index += 1) {
+	let option = document.createElement('option');
+
+	option.innerHTML = estados[index];
+
+	estado.appendChild(option);
+}
+
+$('.datepicker').pickadate()
