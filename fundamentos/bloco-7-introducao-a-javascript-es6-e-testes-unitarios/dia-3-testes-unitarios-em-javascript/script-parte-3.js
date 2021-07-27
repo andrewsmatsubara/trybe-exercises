ba05e16822e3831dfd1249287fa0a70b03189ba4
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-07-27 06:12:18 -0300

    Exercicio 1 Parte 3 alterado

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/script-parte-3.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/script-parte-3.js
@@ -1,18 +1,18 @@
 const assert = require('assert');
 
 const greetPeople = (people) => {
-    let greeting = 'Hello ';
-  
-    for (const person in people) {
-      greeting += people[person];
-    }
-    return greeting;
-  };
-  
-  const parameter = ['Irina', 'Ashleigh', 'Elsa'];
-  const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+  let greeting = [];
 
-  assert.strictEqual(typeof greetPeople, 'function');
+  for (const person in people) {
+    greeting.push(`Hello ${people[person]}`);
+  }
+  return greeting;
+};
 
-  const output = greetPeople(parameter);
-  assert.deepStrictEqual(output, result);
+const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+
+assert.strictEqual(typeof greetPeople, 'function');
+
+const output = greetPeople(parameter);
+assert.deepStrictEqual(output, result);
