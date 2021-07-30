a7ad938bfc202c6dca28573c35ad18eb9edeb293
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-07-30 18:45:41 -0300

    Exercicio 1 realizado

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -64,3 +64,19 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = [
+    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+    'Fundação - Ficção Científica - Isaac Asimov',
+    'Duna - Ficção Científica - Frank Herbert',
+    'A Coisa - Terror - Stephen King',
+    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  ];
+  
+  function formatedBookNames() {
+    // escreva seu código aqui
+    return books.map((livro) => `${livro.name} - ${livro.genre} - ${livro.author.name}`);
+  }
+  
+  assert.deepStrictEqual(formatedBookNames(), expectedResult);
