7d50564c479b546917f133e69c967f4b14b9ef54
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-08-03 19:13:12 -0300

    Exercicio 2 realizado

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio2.js
@@ -2,6 +2,8 @@ const assert = require('assert');
 
 // escreva sum abaixo
 
+const sum = (...num) => num.reduce(((acc, valor) => acc + valor), 0);
+
 assert.strictEqual(sum(), 0);
 assert.strictEqual(sum(1), 1);
 assert.strictEqual(sum(1, 2), 3);
