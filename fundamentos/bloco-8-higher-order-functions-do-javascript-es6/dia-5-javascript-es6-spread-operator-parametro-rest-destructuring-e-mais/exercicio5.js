1d9ace11de61c1434cc18e390d1fb8051d80ddab
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-08-03 19:55:37 -0300

    Exercicio 5 realizado

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio5.js
@@ -4,8 +4,12 @@ const myList = [1, 2, 3];
 
 // escreva swap abaixo
 
+const swap = ([a, b, c]) => [c, b, a];
+
 const swappedList = swap(myList);
 
 assert.strictEqual(swappedList[0], 3);
 assert.strictEqual(swappedList[1], 2);
 assert.strictEqual(swappedList[2], 1);
+
+//Ideia para realizar o exercício retirado do site: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
