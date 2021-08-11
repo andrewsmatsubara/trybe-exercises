d52c90bbdc1186bbb7a8f24cb863aa4aea610f61
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-08-11 20:53:01 -0300

    Exercicio 2 em andamento: Primeira tentativa de estruturaçao do Promise

--- fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/exercicio2.js
+++ fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/exercicio2.js
@@ -1,1 +1,18 @@
-const promise = new Promise(numero => numero = Math.abs(Math.ceil(Math.random() * (50 - 1) + 1)));
+const funcao = () => {
+    const arr = [];
+
+    const promise = new Promise((resolve, reject) => {
+        for (let i = 0; i < 10; i += 1) {
+            const randomNum = Math.abs(Math.ceil(Math.random() * (50 - 1) + 1))
+
+            arr[i] = randomNum;
+        }
+        const somaArr = arr.reduce((acc, curr) => acc + curr, 0);
+
+        (somaArr < 8000) ? resolve() : reject();
+    });
+
+    promise
+        .then = () => console.log('Promise resolvida')
+            .catch = () => console.log('Promise rejeitada')
+}
