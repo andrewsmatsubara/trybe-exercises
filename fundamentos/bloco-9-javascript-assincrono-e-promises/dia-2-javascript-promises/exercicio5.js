0cc6f01a6ac28459f3d2090ce5c1ae4d023c8d80
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-08-12 02:38:33 -0300

    Exercicio 5 realizado

---
+++ fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/exercicio5.js
@@ -0,0 +1,20 @@
+const funcao = () => {
+    const arr = [];
+
+    const promise = new Promise((resolve, reject) => {
+        for (let i = 0; i < 10; i += 1) {
+            const randomNum = Math.pow(Math.ceil(Math.random() * (50 - 1) + 1), 2);
+
+            arr[i] = randomNum;
+        }
+        const somaArr = arr.reduce((acc, curr) => acc + curr, 0);
+
+        (somaArr < 8000) ? resolve(somaArr) : reject();
+    });
+
+    promise
+        .then(somaArr => [2, 3, 5, 10].map(numero => somaArr / numero))
+        .then((somaArr) => somaArr.reduce((acc, curr) => acc + curr), 0)
+        .catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
+}
+funcao();
