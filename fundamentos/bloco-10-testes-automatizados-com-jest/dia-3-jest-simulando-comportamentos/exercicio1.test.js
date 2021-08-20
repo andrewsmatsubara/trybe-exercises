8bc6bd533c4c6388c1a741a1e32a181291c5e5da
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-08-20 15:16:18 -0300

    Atualizando exercicio 1

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/exercicio1.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/exercicio1.test.js
@@ -1,10 +1,7 @@
-const { test, expect } = require('@jest/globals');
 const exercicio1 = require('./exercicio1');
 
-test('Testa se a função foi chamada', () => {
-    exercicio1.geraNumeroAleatorio = jest.fn().mockReturnValue(10);
+test('Testa se a função foi chamada, qual seu retorno e quantas vezes foi chamada', () => {
+    exercicio1.geraNumeroAleatorio = jest.fn().mockReturnValue();
     exercicio1.geraNumeroAleatorio();
-    expect(exercicio1.geraNumeroAleatorio).toHaveBeenCalled();
-    expect(exercicio1.geraNumeroAleatorio()).toBe(10);
-    expect(exercicio1.geraNumeroAleatorio).toHaveBeenCalledTimes(1);
+    expect(exercicio1.geraNumeroAleatorio()).
 });
