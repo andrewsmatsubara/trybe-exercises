d9f0a0caafb03d5df7571d471efddd2115f3e94f
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-08-19 22:26:53 -0300

    Exercicio 4 realizado

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/script4.test.js
@@ -0,0 +1,9 @@
+const getRepos = require('./script4');
+
+test("verifica se os repositórios 'sd-01-week4-5-project-todo-list' e 'sd-01-week4-5-project-meme-generator' se encontram na lista do repositório", () => {
+    const url = 'https://api.github.com/orgs/tryber/repos';
+    return getRepos(url).then((projeto) => {
+        expect(projeto).toContain('sd-01-week4-5-project-todo-list');
+        expect(projeto).toContain('sd-01-week4-5-project-meme-generator');
+    })
+});
