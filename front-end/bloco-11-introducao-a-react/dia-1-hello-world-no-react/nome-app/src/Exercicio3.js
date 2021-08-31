111da1a1b5c26979b3b6ed32d52ab2f6e6714ffe
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-08-30 21:24:10 -0300

    Exercicio 3.2 realizado

--- front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/Exercicio3.js
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/Exercicio3.js
@@ -2,3 +2,18 @@
 
 // Antes: const JSX = <div></div>;
 // Depois: const JSX = <h1>Hello JSX!</h1>;
+
+// Exercício 3.2
+
+// const JSX = <div>
+//     <h1>bla</h1>
+//     <p>ble</p>
+//     <ul>
+//         <li>bli</li>
+//         <li>blo</li>
+//         <li>blu</li>
+//     </ul>
+// </div>
+
+// Exercício 3.3
+
