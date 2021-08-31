0123b11098fadfebcc7afabc3b7bf0848359d203
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-08-30 21:53:23 -0300

    Exercicio 3.9 realizado. A questao 3 foi totalmente resolvida

--- front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/Exercicio3.js
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/Exercicio3.js
@@ -79,3 +79,31 @@
 // 		// Change code above this line
 // 	}
 // };
+
+// Exercício 3.9 
+
+// const ChildComponent = () => {
+// 	return (
+// 		<div>
+// 			<p>I am the child</p>
+// 		</div>
+// 	);
+// };
+
+// class ParentComponent extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 	}
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>I am the parent</h1>
+// 				{ /* Change code below this line */}
+
+// 				<ChildComponent />
+
+// 				{ /* Change code above this line */}
+// 			</div>
+// 		);
+// 	}
+// };
