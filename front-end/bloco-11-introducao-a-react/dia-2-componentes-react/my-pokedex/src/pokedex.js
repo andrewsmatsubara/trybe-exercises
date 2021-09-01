09ec0c3ddfa89dc4abc1a2987b6ff133bbbc4546
Andrews Matsubara <andrewsmatsubara@gmail.com>
2021-08-31 23:45:36 -0300

    Finalizaçao da primeira estruturaçao do componente Pokedex

--- front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/pokedex.js
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/pokedex.js
@@ -2,7 +2,14 @@ import React from 'react';
 import Pokemon from './pokemon';
 
 class Pokedex extends React.Component {
-    
+    render() {
+        const { pokemons } = this.props;
+        return (
+            <div>
+                {pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+            </div>
+        );
+    }
 }
 
 export default Pokedex;
