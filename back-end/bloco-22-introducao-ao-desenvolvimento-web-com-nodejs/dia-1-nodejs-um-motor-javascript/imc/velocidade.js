00f02721206a3b7c6546cf54a09c4fbe721d1583
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-01-09 19:31:14 -0300

    Exercicio 8 realizado

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc/velocidade.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc/velocidade.js
@@ -1,2 +1,8 @@
-const distancia = ;
-const tempo = ;
+const readline = require('readline-sync');
+
+const distancia = readline.questionInt('Digite uma distância: ');
+const tempo = readline.questionInt('Digite o tempo de locomoção: ');
+
+const velocidade = (distancia, tempo) => distancia / tempo;
+
+console.log(velocidade(distancia, tempo));
