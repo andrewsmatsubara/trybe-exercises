3f12e957f1c14ea244974db333371f4a7626e516
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-01-14 13:53:31 -0300

    Exercicio 3 realizado

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -1,6 +1,9 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+
+app.use(bodyParser.json())
 
 app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong' });
@@ -9,6 +12,14 @@ app.get('/ping', (_req, res) => {
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.status(200).json({ "message": `Hello, ${name}!` });
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if (age > 17) return res.status(200).json({ "message": `Hello, ${name}!` });
+
+  return res.status(401).json({ "message": "Unauthorized" });
 });
 
 app.listen(3001);
