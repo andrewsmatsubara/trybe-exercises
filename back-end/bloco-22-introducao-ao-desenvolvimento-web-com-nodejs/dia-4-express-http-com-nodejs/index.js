a7dc0963c852f3a65ff417cd08195d8a2c4e912e
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-01-14 14:07:26 -0300

    Exercicio 4 realizado

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -22,4 +22,9 @@ app.post('/greetings', (req, res) => {
   return res.status(401).json({ "message": "Unauthorized" });
 });
 
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` })
+});
+
 app.listen(3001);
