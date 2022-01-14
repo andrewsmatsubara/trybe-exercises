93c746284e5befed6bbc1fe5416599cbeca34aba
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-01-14 13:42:22 -0300

    Exercicio 2 realizado

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong' });
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ "message": `Hello, ${name}!` });
 });
 
 app.listen(3001);
