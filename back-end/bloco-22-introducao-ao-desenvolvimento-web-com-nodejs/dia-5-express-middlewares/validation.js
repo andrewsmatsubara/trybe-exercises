789373a041c0d82b0fdc7a71b3502cca68697f50
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-01-15 03:34:00 -0300

    Alterando o regex para validaçao de email

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/validation.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/validation.js
@@ -1,7 +1,7 @@
 const usernameValidation = (req, res, next) => {
   const { username } = req.body;
 
-  if (!(username.length > 3)) return res.status(400).json({ "message": "invalid data" });
+  if (!(username.length > 3)) return res.status(400).json({ "message": "invalid username" });
 
   next();
 }
@@ -9,9 +9,9 @@ const usernameValidation = (req, res, next) => {
 const emailValidation = (req, res, next) => {
   const { email } = req.body;
 
-  const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+  const regex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 
-  if (!(regex.test(email))) return res.status(400).json({ "message": "invalid data" });
+  if (!(regex.test(email) === true)) return res.status(400).json({ "message": "invalid email" });
 
   next();
 }
@@ -20,7 +20,7 @@ const passwordValidation = (req, res, next) => {
   const { password } = req.body;
   const regex = /^[0-9]*$/;
 
-  if (!(password >= 4 && password <= 8 && regex.test(password))) return res.status(400).json({ "message": "invalid data" });
+  if (!(password.length >= 4 || password.length <= 8 || regex.test(password) === true)) return res.status(400).json({ "message": "invalid password" });
 
   next();
 }
