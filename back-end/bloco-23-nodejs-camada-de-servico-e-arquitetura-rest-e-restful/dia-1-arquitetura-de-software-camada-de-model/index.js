01c938822741fbccd97085da85c07894f1020acc
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-01-22 17:51:21 -0300

    Retomando exercícios

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const create = require('./models/Users');
 
 app.use(bodyParser.json());
 
-app.post('/user');
+app.post('/user', create);
 
 app.listen(3000, () => {
   console.log('Atualmente trabalhando na porta 3000');
