587f15c21ee12e61369ddd0a3e385071ee9a2f39
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-03-05 03:25:10 -0300

    Primeira parte da resoluçao dos exercicios realizado

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "andrewsmatsubara",
+    "password": "S@p0l3g4l",
+    "database": "db_24.1",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
